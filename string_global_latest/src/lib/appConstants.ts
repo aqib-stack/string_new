3b6ee16cd7df8579ea8904a7c1bd3a58f5c3c269
Muhammad Aqib <aqib@Muhammads-MacBook-Pro.local>
2026-03-28 04:10:50 +0500

    Remove string_global_latest folder from repo

--- string_global_latest/src/lib/appConstants.ts
+++
@@ -1,5 +0,0 @@
-export const SHARED_SHOP_ID = 'demo-shop-1';
-export const SHARED_SHOP_NAME = 'Central Court Tennis Lab';
-export const AUTH_USERS_KEY = 'stringglobe_auth_users';
-export const AUTH_USERS_BACKUP_KEY = 'stringglobe_auth_users_backup';
-export const AUTH_SESSION_KEY = 'stringglobe_auth_session';
